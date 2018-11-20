65a19758d83b89395d10f2d5121fbb24e746169b
Ankita <muddapurankita29@gmail.com>
2018-11-20 12:29:04 +0530

    Add project files.

---
+++ WebApplication12/Controllers/CustomerModelController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication12.Models;
+
+namespace WebApplication12.Controllers
+{
+    public class CustomerModelController : Controller
+    {
+        // GET: CustomerModel
+        public ActionResult Index()
+        {
+            List<CustomerModel> list = new List<CustomerModel>();
+            list.Add(new CustomerModel(){ Name
+                = "Ankita", Address = "Muddapur", Email_Id = "muddapurankita29@gmail.com", Phone_No = "0987654132", Gender = "female" });
+            list.Add(new CustomerModel()
+            {
+                Name
+                = "Anisha",
+                Address = "Bhosale",
+                Email_Id = "anishabhosale@gmail.com",
+                Phone_No = "0987654132",
+                Gender = "female"
+            });
+            list.Add(new CustomerModel()
+            {
+                Name
+                = "Bahveshwari",
+                Address = "Bhambar",
+                Email_Id = "bahveshwaribhambar@gmail.com",
+                Phone_No = "0987654132",
+                Gender = "female"
+            });
+            list.Add(new CustomerModel()
+            {
+                Name
+                = "Nikita",
+                Address = "More",
+                Email_Id = "nikitamore@gmail.com",
+                Phone_No = "0987654132",
+                Gender = "female"
+            });
+
+            return View("Index",list);
+        }
+    }
+}
---
+++ WebApplication12/Models/CustomerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication12.Models
+{
+    public class CustomerModel                
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone_No { get; set; }
+        public string Email_Id {get;set; }
+        public string Gender { get; set; }
+
+    }
+}
